3ab8af2302953bc97255ab0225562ab36ba9178d
Himansh8 <132002811+Himansh8@users.noreply.github.com>
2023-09-19 17:15:52 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,26 @@
+"use strict";
+var firstname="Himanshu sharma";
+console.log(firstname);
+firstname="Aman";
+console.log(firstname);
+
+//constant decelaration
+const pi=22/7;
+console.log(pi*2*2);
+
+//string indexing
+let str="himanshu sharma";
+console.log(str[2]);
+
+//length of a string
+console.log(str.length);
+
+//last index
+console.log(str[str.length-1]);
+
+// trim()
+
+let str1="   Himanshu_sharma    ";
+console.log(str1.length);
+let str2=str1.trim();
+console.log(str2.length);
